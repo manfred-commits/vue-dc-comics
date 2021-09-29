d2b77fd58b878c69036445d99cacccdfdfc2fe5a
manfred-commits <manfredipiraino@gmail.com>
2021-09-29 23:23:12 +0200

    added footer top

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,166 @@
 <template>
     <footer>
-        <section class="general-content-formatting">
-            
-        </section>
+        <div class="general-content-formatting">
+
+            <section class="footer-top">
+                <div class="footer-top-menus">
+                <div class="first-menu">
+                    <div class="menu-top-block">
+                    <h4>DC COMICS</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Characters
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Comics
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Movies
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                TV
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Games
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Videos
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                News
+                            </a>
+                        </li>
+                    </ul>
+                    </div>
+
+                    <div class="menu-bottom-block">
+                    <h4>SHOP</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Shop DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop DC Collectibles
+                            </a>
+                        </li>
+                    </ul>
+
+                    </div>
+                </div>
+                
+                <div class="menu">
+                    <h4>DC</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Terms Of Use
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Privacy policy (New)
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ad Choices
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Advertising
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Jobs
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Subscriptions
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Talent Workshops
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                CPSC Certificates
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ratings
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop Help
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="menu">
+                    <h4>SITES</h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                DC
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                MAD Magazine
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Kids
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Universe
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Power Visa
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+                
+            </section>
+            <section class="footer-bottom">
+
+            </section>
+        </div>
     </footer>
 </template>
 
@@ -14,4 +172,66 @@ export default {
 
 <style lang="scss" scoped>
 
+footer{
+    background-image: url('../assets/img/footer-bg.jpg');
+    background-position: center top;
+
+    .general-content-formatting{
+        flex-grow: 1;
+        height: 100%;
+        background-image: url('../assets/img/dc-logo-bg.png');
+        background-repeat: no-repeat;
+        background-position: center right;
+
+        .footer-top{
+            display: flex;
+            align-items: flex-start;
+            justify-content: space-between;
+
+
+            .footer-top-menus{
+                display: flex;
+                flex-wrap: wrap;
+                align-items: flex-start;
+                width: 50%;
+                padding: 0 0 2.8125rem;
+                padding-left: 0rem;
+
+
+
+                .first-menu{
+                    padding: .625rem 0;
+                    padding-right: .9375rem;
+                }
+                .menu{
+                    padding: .625rem 1.25rem;
+                }
+                h4{
+                    font-size: 1.25rem;
+                    padding: .8125rem;
+                    padding-left: 0rem;
+
+                    color:white;
+                }
+                ul{
+                    list-style: none;
+
+                    li{
+                        
+
+                        a{
+                            text-decoration: none;
+                            color: grey;
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+        
+    }
+
+}
 </style>
