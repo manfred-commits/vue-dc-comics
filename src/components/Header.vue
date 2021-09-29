178949b2c0908bafdceb0e512d6562f2b69e2dd3
manfred-commits <manfredipiraino@gmail.com>
2021-09-29 17:54:39 +0200

    added header component with style & v-for

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,137 @@
 <template>
     <header>
-        
+        <nav>
+            <div class="container-img">
+                <img src="@/assets/img/dc-logo.png" alt=" Detective Comics small logo">
+            </div>
+            
+            <ul>
+                <li v-for="(component,index) in nav" :key="index" :class="{active : component.current==true}">
+                    <a :href="component.url">{{component.name.toUpperCase()}}</a>
+                </li>
+            </ul>
+            
+        </nav>
     </header>
 </template>
 
 <script>
 export default {
-    name:'Header'
+    name:'Header',
+    data() {
+        return {
+            nav:    [
+                {
+                    name:"characters",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"comics",
+                    url:"#",
+                    current:true
+                },
+                {
+                    name:"movies",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Tv",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Games",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Collectibles",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Videos",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Fans",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"News",
+                    url:"#",
+                    current:false
+                },
+                {
+                    name:"Shop",
+                    url:"#",
+                    current:false
+                },
+            ]
+            
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
+header{
+    margin: 0rem 0rem 1.25rem;
 
+    nav{
+        display: flex;
+        align-items: center;
+        width: 100%;
+        justify-content: space-between;
+        font-weight: bold;
+        
+        .container-img{
+            
+            height: 100%;
+            padding: .9375rem;
+            padding-left: 0;
+
+            img{
+                object-fit: cover;
+                height: 100%;
+                width: 100%;
+
+            }
+
+            
+        }
+        ul{
+            display: flex;
+            list-style: none;
+
+            li:last-child{
+               padding-right: 0rem; 
+            }
+
+            li{
+                padding: 1.25rem;
+
+                &.active{
+                    color: $active_color;
+                    border-bottom: .1875rem solid $active_color;
+
+                    a{
+                    color: $active_color;
+
+                    }
+                }
+
+                a{
+                    color: $nav_primary_color;
+                    text-decoration: none;
+                }
+            }
+        }
+    }
+}
 </style>
