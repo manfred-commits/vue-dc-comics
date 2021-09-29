6a9192d6c24ad10e99d4cf35742fe6beba8f8be6
manfred-commits <manfredipiraino@gmail.com>
2021-09-29 18:13:44 +0200

    added basic main component structure

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,14 @@
 <template>
     <main>
+        <div class="general-content-formatting">
+            <section class="content">
+                <h2>-->Content goes here </h2>
+            </section>
+
+            <section class="site-features">
+
+            </section>
+        </div>
 
     </main>
 </template>
@@ -11,5 +20,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '../style/variables.scss';
 
+    main{
+        h2{
+            color: white;
+            padding: 1.25rem 0;
+        }
+        .site-features{
+            background-color: $site-features_primary_background_color;
+        }
+    }
 </style>
