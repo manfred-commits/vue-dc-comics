eccb227e52bc10638968f9fe3555a0d3da3ff973
manfred-commits <manfredipiraino@gmail.com>
2021-09-30 16:13:11 +0200

    added basic Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card-container">
+        <img :src="card.thumb" :alt="card.type">
+        <h5>{{card.series}}</h5>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+
+    props: ['card']
+}
+</script>
+
+<style lang="scss" scope>
+.card-container{
+    width: 100% /6;
+
+    img{
+        object-fit: cover;
+        object-position: center;
+        width: 100%;
+    }
+}
+</style>
